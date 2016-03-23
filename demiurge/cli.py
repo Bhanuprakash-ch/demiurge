29d130c95ba74c668fbd5f214c1696035c413238
Maciej Strzelecki <maciej.strzelecki@intel.com>
2016-03-23 10:45:10 +0100

    Increase readability

--- demiurge/cli.py
+++ demiurge/cli.py
@@ -28,24 +28,25 @@ AUTH = HTTPBasicAuth()
 USERS = {}
 
 @AUTH.get_password
-def get_pw(username):
-    if username in USERS:
-        return USERS.get(username)
-    else:
-        return None
+def get_password(username):
+    return USERS.get(username)
 
 BOTO3_CLIENT_KWARGS = {}
 STACK_PARAMETERS = []
 
 @click.command()
-@click.option('--username', envvar='USERNAME', required=True)
-@click.option('--password', envvar='PASSWORD', required=True)
+@click.option('--username', envvar='USERNAME', required=True,
+              help='Username for basic authentication.')
+@click.option('--password', envvar='PASSWORD', required=True,
+              help='Password for basic authentication.')
+
 @click.option('--region-name', envvar='AWS_DEFAULT_REGION', default='us-west-2')
 @click.option('--aws-access-key-id', envvar='AWS_ACCESS_KEY_ID')
 @click.option('--aws-secret-access-key', envvar='AWS_SECRET_ACCESS_KEY')
 @click.option('--vpc', envvar='VPC', required=True)
 @click.option('--subnet', envvar='SUBNET', required=True)
 @click.option('--key-name', envvar='KEY_NAME', required=True)
+
 @click.option('--debug/--no-debug', '-d', default=False)
 @click.option('--port', '-p', envvar='PORT', default=8080)
 def cli(username, password, port, debug, **kwargs):
@@ -62,7 +63,7 @@ def cli(username, password, port, debug, **kwargs):
 
     app = connexion.App(__name__, specification_dir='swagger/', arguments={'version': __version__},
                         debug=debug)
-    app.add_api('clusters.yaml', resolver=RestyResolver('kube_tap.api'))
+    app.add_api('clusters.yaml', resolver=RestyResolver('demiurge.api'))
     app.run(port=port)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 colorcolumn=100
