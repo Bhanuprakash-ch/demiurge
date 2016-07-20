86a4f5c5c96b5c4125dd544425f44a23fa642e17
Piotr <piotr.a.chmielinski@intel.com>
2016-07-20 16:23:42 +0200

    DPNG-9114 add versions to dependencies
    Conflicts:
    	demiurge/__init__.py
    	setup.py

--- setup.py
+++ setup.py
@@ -18,17 +18,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='demiurge',
-    version='0.7.0',
+    version='0.7.1',
     packages=find_packages(),
     install_requires=[
-        'awacs',
-        'boto3',
-        'click',
-        'connexion',
-        'fauxfactory',
-        'Flask-HTTPAuth',
-        'troposphere',
-        ],
+            'awacs==0.5.4',
+            'boto3==1.3.1',
+            'click==6.6',
+            'connexion==1.0.103',
+            'fauxfactory==2.0.9',
+            'Flask-HTTPAuth',
+            'troposphere==1.6.0',
+            'pyopenssl',
+            'cffi==1.7.0'
+            ],
     author='Maciej Strzelecki',
     author_email='maciej.strzelecki@intel.com',
     license='Apache License, Version 2.0',
